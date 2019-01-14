b855410a8c54a7ddf6764308f18232d22941aa6b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-14 23:35:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
 
-    if @user != nil:NilClass
+    if @user != nil
       session[:id] = @user.id
       redirect '/users/home'
     else
